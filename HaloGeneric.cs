c1d7bfde372f0070343936c5416792c0f0ef2722
kafka <kafkariyadi691@gmail.com>
2025-03-15 22:27:07 +0700

    Menambahkan class HaloGeneric dengan method SapaUser

---
+++ HaloGeneric.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul5_2311104041
+{
+    class HaloGeneric
+    {
+        public static void SapaUser<T>(T user)
+        {
+            Console.WriteLine($"Halo user {user}");
+        }
+
+        static void Main()
+        {
+            SapaUser("Kafka");
+        }
+    }
+}
+
